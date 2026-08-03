d875c62b5070f8a231ff3a33db1abaf2252f02b3
pilgrim <449140441@qq.com>
2026-08-03 17:04:30 +0800

    Enable ambient music autoplay

--- frontend-react/src/components/music/AmbientMusicPlayer.tsx
+++ frontend-react/src/components/music/AmbientMusicPlayer.tsx
@@ -1,6 +1,6 @@
 import './AmbientMusicPlayer.css';
 
-const PLAYER_URL = 'https://music.163.com/outchain/player?type=2&id=1803814925&auto=0&height=66';
+const PLAYER_URL = 'https://music.163.com/outchain/player?type=2&id=1803814925&auto=1&height=66';
 
 function AmbientMusicPlayer() {
   return (
